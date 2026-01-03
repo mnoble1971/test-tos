3b9ebaff85339159bee5a9fb001c81836e9b4dd7
Michael Noble <michaelhnoble@gmail.com>
2026-01-03 09:27:09 -0500

    u

--- docker_install.sh
+++ docker_install.sh
@@ -1,4 +1,5 @@
 dnf install -y dnf-plugins-core ca-certificates curl gnupg lvm2
-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 systemctl enable --now docker
+# test with docker run hello-world
